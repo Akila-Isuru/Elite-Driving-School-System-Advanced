3a892bf6cdc58b42e967145aa24d1c3692c10832
akilaisuru <akilaisurukahingala@gmail.com>
2025-10-02 21:56:48 +0530

    adding dependencies

---
+++ src/main/java/org/example/elitedrivinglearnersadvancedapp/AppInitializer.java
@@ -0,0 +1,4 @@
+package org.example.elitedrivinglearnersadvancedapp;
+
+public class AppInitializer {
+}
---
+++ src/main/java/org/example/elitedrivinglearnersadvancedapp/entity/Lesson.java
@@ -0,0 +1,4 @@
+package org.example.elitedrivinglearnersadvancedapp.entity;
+
+public class Lesson {
+}
